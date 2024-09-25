86a837b67200e093397a063f3f30e014b5d68857
jonas <147396302+jonasbrik@users.noreply.github.com>
2024-09-25 08:30:36 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ function criaCartao(categoria, pergunta, resposta){
 }
 function criaCartao(categoria, pergunta, resposta){
     let container = documento.getElementById('container')
+    let cartao= document.createElement('article')
+    let responsaEstaVisivel= false
+}
+criaCartao.innerHTML=`
+<div class="cartao_conteudo">
+<!-- Código omitído... -->
+</div>
+`
+container.appendChild(cartao)
+
+function viraCatao(){
+    respostaestaVisivel= !responsaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
 }
