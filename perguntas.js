95cb2034d9c3d083f32898f48e656296b9e11cf7
jonas <147396302+jonasbrik@users.noreply.github.com>
2024-09-25 08:17:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+   'Programação',
+   'O que Python',
+   'O Python é uma lingagem de programação'
+)
+criaCartao(
+   'Geografia',
+   'Qual a capital da França?',
+   'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
